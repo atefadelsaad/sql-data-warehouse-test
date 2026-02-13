3b86a6cdcbfa97843eab58071eba452a5b61dd98
Atef Adel <atefadel1996123@gmail.com>
2026-02-13 05:37:45 -0800

    Create bronze.sp_load_creditcard.sql

---
+++ schemas/bronze.sp_load_creditcard.sql
@@ -0,0 +1,20 @@
+CREATE OR ALTER PROCEDURE bronze.load_creditcard
+AS 
+ BEGIN
+	INSERT INTO test.bronze.erp_creditcard(
+		CreditCardID,
+		CardType,
+		CardNumber, 
+		ExpMonth, 
+		ExpYear,
+		ModifiedDate 
+	   )
+     SELECT 
+		CreditCardID,
+		CardType,
+		CardNumber, 
+		ExpMonth, 
+		ExpYear,
+		ModifiedDate 
+	 FROM AdventureWorks2022.Sales.CreditCard
+ END
